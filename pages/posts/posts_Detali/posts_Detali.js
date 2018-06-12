fa2cdc742cfcd5c5d1430bfa9dde208b8d645d8b
zhangjie <zhangjie7467@163.com>
2018-06-12 20:03:50 +0800

    y

--- pages/posts/posts_Detali/posts_Detali.js
+++ pages/posts/posts_Detali/posts_Detali.js
@@ -1,14 +1,12 @@
 // pages/posts/posts_Detali/posts_Detali.js
 var postsData = require("../../../data/posts_data.js")
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
     
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
