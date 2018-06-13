e36a85acbd8eda76d2f56b233c8435cb91c2b289
zj <zh>
2018-06-13 11:10:29 +0800

    收藏提示框

--- pages/posts/posts_Detali/posts_Detali.js
+++ pages/posts/posts_Detali/posts_Detali.js
@@ -11,33 +11,49 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var postsData1 = postsData.contetn[options.id]
+    //获取新闻详情的ID
+    var postsId = options.id;
+    //获取每个ID的书记
+    var postsData1 = postsData.contetn[postsId]
+   //保存ID
     this.setData({
-      postsId: options.id
+      postsId: postsId
     })
+     //设置数据数据
     this.setData({ postsData: postsData1 })
     var postsCollected=wx.getStorageSync("postsCollected")
+    //判断是都有缓存的数据
     if (postsCollected){
-      var postsCollected = postsCollected[options.id]
+      var postsCollected = postsCollected[postsId]
       this.setData({
         collected: postsCollected
       })
     }else {
       var postsCollected={};
-      postsCollected[options.id]=false;
-      wx.getStorageSync("postsCollected", postsCollected);
+      postsCollected[postsId]=false;
+      console.log(postsCollected)
+      wx.setStorageSync("postsCollected", postsCollected);
     }
 
   },
   onCollctedTap:function (event) {
-  
+    //获得是否收藏数据
     var postsCollected = wx.getStorageSync("postsCollected");
-    var postsCollected = postsCollected[this.data.postsId];
-    // console.log(postsCollected)
-    postsCollected = !postsCollected;
-    postsCollected[this.data.postsId] = postsCollected;
-    wx.setStorageSync(postsCollected, postsCollected)
-    this.setData({ collected:postsCollected})
+    //找到当前的收藏书记
+    var postsCollectedSelect = postsCollected[this.data.postsId];
+    //收藏与为收藏切换数据
+    postsCollectedSelect = !postsCollectedSelect;
+    postsCollected[this.data.postsId] = postsCollectedSelect;
+    //缓存是否收藏数据
+    wx.setStorageSync("postsCollected", postsCollected);
+    //保存是否输出数据 界面展示数据
+    this.setData({ collected: postsCollectedSelect});
+    // 收藏取消显示弹出框
+    wx.showToast({
+      title: postsCollectedSelect?'收藏成功':"取消收藏",
+      icon: postsCollectedSelect ? 'success' : "none",
+      duration: 1000
+    })
   },
   
   /**
